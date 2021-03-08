d3299f88aa8c3bdb64ca1f3dbfa6520a6e1ed8d0
DrErickson <77122859+DrErickson@users.noreply.github.com>
2021-03-08 09:03:45 -0800

    Initial commit

---
+++ TreeT.cpp
@@ -0,0 +1,166 @@
+
+#include "TreeT.h"
+
+
+template<class T>
+TreeT<T>::TreeT() {
+    root = nullptr;
+    numNodes = 0;
+}
+
+template<class T>
+TreeT<T>::~TreeT() {
+    DestroyTree(root);
+}
+
+template<class T>
+void TreeT<T>::DestroyTree(TreeT::Node *node) {
+    if (node == nullptr)
+        return;
+
+    DestroyTree(node->left);
+    DestroyTree(node->right);
+    delete node;
+}
+
+
+template<class T>
+void TreeT<T>::Add(T value) {
+
+    if (root == nullptr) {
+        root = new Node;
+        numNodes++;
+        return;
+    }
+
+    AddHelper(root, value);
+
+}
+
+template<class T>
+void TreeT<T>::AddHelper(TreeT::Node *&subtree, T value) {
+
+    if (subtree == nullptr) {
+        subtree = new Node;
+        subtree->value = value;
+        numNodes++;
+        return;
+    }
+
+    if (value < subtree->value) {
+        AddHelper(subtree->left);
+    } else if (value > subtree->value) {
+        AddHelper(subtree->right);
+    } else {
+        return;
+    }
+}
+
+template<class T>
+void TreeT<T>::Remove(T item) {
+    RemoveHelper(root, item);
+}
+
+template<class T>
+void TreeT<T>::RemoveHelper(Node*& subtree, T value) {
+
+    if (value == subtree->value) {
+        DeleteNode(subtree);  // Found the node. Now delete it.
+    }
+    else if (value < subtree->value) {
+        subtree = subtree->left;    // go left
+    }
+    else {
+        subtree = subtree->right;   // go right
+    }
+}
+
+template<class T>
+void TreeT<T>::DeleteNode(Node *&subtree) {
+
+    T item;
+    Node* tempPtr;
+
+    tempPtr = subtree;
+    if (subtree->right == NULL && subtree->left == NULL)
+    {
+        delete tempPtr;
+        subtree = NULL;
+    }
+    else if (subtree->left == NULL)
+    {
+        subtree = subtree->right;
+        delete tempPtr;
+    }
+    else if (subtree->right == NULL)
+    {
+        subtree = subtree->left;
+        delete tempPtr;
+    }
+    else {
+        GetPredecessor(subtree->left, item);
+        subtree->info = item;
+        RemoveHelper(subtree->left, item);
+    }
+}
+
+
+template<class T>
+void TreeT<T>::GetPredecessor(TreeT::Node *curr, T& value) {
+    while (curr->right != nullptr) {
+        curr = curr->right;
+    }
+    value = curr->value;
+}
+
+template<class T>
+bool TreeT<T>::Contains(T value) {
+    return ContainsHelper(root, value);
+}
+
+// Used for recursive version.
+template<class T>
+bool TreeT<T>::ContainsHelper(Node* subtree, T value) {
+
+}
+
+template<class T>
+int TreeT<T>::Size() {
+    return numNodes;
+}
+
+template<class T>
+void TreeT<T>::ResetIterator(Order traverseOrder) {
+    if (traverseOrder == IN_ORDER)
+        PlaceInOrder(root);
+    else if (traverseOrder == POST_ORDER)
+        PlacePostOrder(root);
+    else
+        PlacePreOrder(root);
+}
+
+template<class T>
+T TreeT<T>::GetNextItem() {
+    return T();
+}
+
+template<class T>
+void TreeT<T>::PlacePreOrder(TreeT::Node *node) {
+
+}
+
+template<class T>
+void TreeT<T>::PlacePostOrder(TreeT::Node *node) {
+
+}
+
+template<class T>
+void TreeT<T>::PlaceInOrder(TreeT::Node *node) {
+
+}
+
+
+
+
+
+
---
+++ TreeT.h
@@ -0,0 +1,62 @@
+//
+// Author: Varick Erickson
+// Date: 2/18/2021
+//
+
+#ifndef TREET_H
+#define TREET_H
+
+#include <queue>
+using namespace std;
+
+// Used to identify the order you wish to traverse the tree
+enum Order {PRE_ORDER, POST_ORDER, IN_ORDER};
+
+template<class T>
+class TreeT {
+public:
+    TreeT();
+    ~TreeT();
+
+    void Add(T value);       // Add value to the tree
+    void Remove(T value);    // Remove value from the tree
+    bool Contains(T value);  // Determines if value is in the tree
+
+    int Size();     // Number of value in the tree
+
+    // These are used by the iterator
+    void ResetIterator(Order traverseOrder);    // Initializes the Iterator
+    T GetNextItem();
+
+private:
+    struct Node {
+        Node* left = nullptr;
+        Node* right = nullptr;
+        T value;
+    };
+    Node* root;
+    int numNodes;
+
+    // Used for de-constructor
+    void DestroyTree(Node* node);
+
+    // Recursive helpers
+    bool ContainsHelper(Node* subtree, T value);
+    void AddHelper(Node*& subtree, T value);
+
+    // Used for Node removal
+    void RemoveHelper(Node*& subtree, T value);
+    void DeleteNode(Node*& subtree);
+    void GetPredecessor(Node* curr, T& value);
+
+    // Used for iterator
+    queue<T> iterArr;     // queue used for the iterator
+    void PlacePreOrder(Node* node);
+    void PlacePostOrder(Node* node);
+    void PlaceInOrder(Node* node);
+};
+
+
+#include "TreeT.cpp"
+
+#endif //TREET_H
